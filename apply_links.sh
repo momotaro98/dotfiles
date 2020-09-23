52e2063a3bf0969ad6b0162593173d8cd9d3b79b
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2020-09-24 00:04:06 +0900

    add git-secrets things

--- apply_links.sh
+++ apply_links.sh
@@ -3,3 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 ln -sf $DIR/.git_template/hooks/prepare-commit-msg ~/.git_template/hooks/prepare-commit-msg
+
+ln -sf $DIR/.git_templates/git-secrets/hooks/commit-msg ~/.git_templates/git-secrets/hooks/commit-msg
+ln -sf $DIR/.git_templates/git-secrets/hooks/pre-commit ~/.git_templates/git-secrets/hooks/pre-commit
+ln -sf $DIR/.git_templates/git-secrets/hooks/pre-commit-msg ~/.git_templates/git-secrets/hooks/prepare-commit-msg
