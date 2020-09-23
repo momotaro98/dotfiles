aed2703fa9727235b425d5af13bc15dcb4aaa531
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2020-09-24 00:19:57 +0900

    update apply shell

--- apply_links.sh
+++ apply_links.sh
@@ -2,6 +2,16 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+ln -sf $DIR/.zshrc ~/.zprezto/runcoms/zshrc
+ln -sf $DIR/.zshenv ~/.zprezto/runcoms/zshenv
+# TODO: zprofile and zpreztorc files
+
+ln -sf $DIR/.vimrc ~/.vimrc
+
+ln -sf $DIR/.gitconfig ~/.gitconfig
+ln -sf $DIR/.gitignore_global ~/.gitignore_global
+ln -sf $DIR/.gitmessage.txt ~/.gitmessage.txt
+
 ln -sf $DIR/.git_template/hooks/prepare-commit-msg ~/.git_template/hooks/prepare-commit-msg
 
 ln -sf $DIR/.git_templates/git-secrets/hooks/commit-msg ~/.git_templates/git-secrets/hooks/commit-msg
