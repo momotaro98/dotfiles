eb174f70d3f6fe136cf998c010c96c1a7e863aa2
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2021-02-13 22:39:58 +0900

    update vscode and its install script

--- .vscode/vscode_install.sh
+++ .vscode/vscode_install.sh
@@ -1,25 +1,39 @@
 #!/bin/sh
 
+echo "Starting symbolic link setting"
+
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 VSCODE_SETTING_DIR=~/Library/Application\ Support/Code/User
 
-rm "$VSCODE_SETTING_DIR/settings.json"
-ln -s "$SCRIPT_DIR/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
+rm "${VSCODE_SETTING_DIR}/settings.json"
+ln -s "${SCRIPT_DIR}/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
 
-rm "$VSCODE_SETTING_DIR/keybindings.json"
-ln -s "$SCRIPT_DIR/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
+rm "${VSCODE_SETTING_DIR}/keybindings.json"
+ln -s "${SCRIPT_DIR}/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
 
-# snippets
-SNIPPETS_DIR=$SCRIPT_DIR/snippets
-VSCODE_SNIPPETS_DIR=$VSCODE_SETTING_DIR/snippets
-# TODO: for loop for all of languages snippets json files
-rm "$VSCODE_SNIPPETS_DIR/go.json"
-ln -s "$SNIPPETS_DIR/go.json" "${VSCODE_SNIPPETS_DIR}/go.json"
+## Snippets
+for filename in snippets/*.json; do
+  # $filename is like "snippets/python.json"
+  rm "${VSCODE_SETTING_DIR}/${filename}"
+  ln -s "${filename}" "${VSCODE_SETTING_DIR}/${filename}"
+done
 
-# install extention
+echo "Finished symbolic link setting"
+
+
+echo "Starting installing extensions"
+
+if ! command -v code &> /dev/null
+then
+  echo "VSCode 'code' command is not found. Install it in VSCode Command Palette (⇧⌘P) then type 'Shell Command: install'"
+  exit
+fi
+
 cat extensions | while read line
 do
  code --install-extension $line
 done
 
 code --list-extensions > extensions
+
+echo "Finished installing extensions"
